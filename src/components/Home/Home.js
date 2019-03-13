10e584a474612a0bb21104163804666f01180127
Alex <Zizzs17@gmail.com>
2019-03-13 16:35:37 -0700

    Rewrote the homepage in preparation for styling overhauls tomorrow. Temporary name added.

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,17 +5,9 @@ class Home extends Component {
   render() {
     return (
       <div>
-        <p id="rpgHeader">Welcome to RPG Game</p>
+        <p id="rpgHeader">[ Void ]</p>
         <div id="changeLog">
-          <p>ChangeLog:</p>
-          <ul>
-            <li>Created new Navbar</li>
-            <li>Set up Routing</li>
-            <li>Created Home, CreateCharacter, HUB, and Navbar Components</li>
-            <li>Created Main Panel, Wrapper, and Zone Components</li>
-            <li>Added Create a Character functionality and Main Panel stats</li>
-            <li>Added intro story in the HUB. Character must progress through the story before the Zones and Pylons are able to be accessed</li>
-          </ul>
+          Create a Character, Then Enter the HUB to explore the void.
         </div>
       </div>
     );
